963877f603a1b194ea9778cbe7699b3b586e9296
MikeCoreTV <mikeasfr1@gmail.com>
2024-08-30 08:11:22 -0400

    Exercise 1.9 Completed
    VSCode still knowing what I want to write more than I do

--- part1/uniCafe/src/App.jsx
+++ part1/uniCafe/src/App.jsx
@@ -1,6 +1,14 @@
 import { useState } from 'react'
 
 const Statistics = ({ good, neutral, bad }) => {
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return (
+      <div>
+        <h2>statistics</h2>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
   return (
     <div>
       <h2>statistics</h2>
