7db4d7e1511feceb1f9f93739b282e3d6d5d5abc
MikeCoreTV <mikeasfr1@gmail.com>
2024-08-30 07:38:11 -0400

    Exercise 1.7 Completed
    VSCode really likes to jump ahead I'm noticing..

--- part1/uniCafe/src/App.jsx
+++ part1/uniCafe/src/App.jsx
@@ -16,6 +16,9 @@ const App = () => {
       <p>good{good}</p>
       <p>neutral{neutral}</p>
       <p>bad{bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {(good - bad) / (good + neutral + bad)}</p>
+      <p>positive {(good / (good + neutral + bad)) * 100} %</p>
     </div>
   )
 }
