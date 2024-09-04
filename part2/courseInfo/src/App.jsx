9153618554c770d81e79bbc1bf352a37bfb4b1cb
MikeCoreTV <mikeasfr1@gmail.com>
2024-09-04 09:27:03 -0400

    Exercise 2.1 Completed

---
+++ part2/courseInfo/src/App.jsx
@@ -0,0 +1,41 @@
+
+const Course = (props) => {
+  return (
+    <div>
+      <h1>{props.course.name}</h1>
+      {props.course.parts.map((part) => <p key={part.id}>{part.name + " " + part.exercises}</p>)}
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return <p>Number of exercises {props.sumOfExercises}</p>
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
