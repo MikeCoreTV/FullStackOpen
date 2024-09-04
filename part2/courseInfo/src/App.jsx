37242d1a3d49b90fae6b1b74be6165787b4e1a64
MikeCoreTV <mikeasfr1@gmail.com>
2024-09-04 09:50:47 -0400

    Exercise 2.3 Completed
    I sadly did not do this initially

--- part2/courseInfo/src/App.jsx
+++ part2/courseInfo/src/App.jsx
@@ -1,11 +1,10 @@
 
 const Course = (props) => {
-  let total = 0
+  const total = props.course.parts.reduce((s, p) => s + p.exercises, 0)
   return (
     <div>
       <h1>{props.course.name}</h1>
       {props.course.parts.map((part) => <p key={part.id}>{part.name + " " + part.exercises}</p>)}
-      {props.course.parts.map((part) =>{total = part.exercises + total})}
       <b>Total of {total} exercises</b>
     </div>
     
